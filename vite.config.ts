3ef069a2fc3e493f7a7d3d523ccb970fef8ba8ff
sjuluru1 <saiteja.juluru@slu.edu>
2025-06-07 01:17:15 -0500

    Added deployement related changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  base: "/julurusaiteja39.github.io/", // CRUCIAL for GitHub Pages
+  build: {
+    outDir: "docs", // Make sure build output goes to ./docs
+    emptyOutDir: true, // Clean old builds from docs
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-}));
+});
