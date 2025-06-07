c4ed86749776619781df2ffb7033f5e94fb1f0c9
sjuluru1 <saiteja.juluru@slu.edu>
2025-06-07 01:26:54 -0500

    tracing issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import {componentTagger} from "lovable-tagger";
+// export default defineConfig({
+//   base: "/julurusaiteja39.github.io/", // CRUCIAL for GitHub Pages
+//   build: {
+//     outDir: "docs", // Make sure build output goes to ./docs
+//     emptyOutDir: true, // Clean old builds from docs
+//   },
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+// });
 
-export default defineConfig({
-  base: "/julurusaiteja39.github.io/", // CRUCIAL for GitHub Pages
-  build: {
-    outDir: "docs", // Make sure build output goes to ./docs
-    emptyOutDir: true, // Clean old builds from docs
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
   },
-  plugins: [react()],
+  base: "/julurusaiteja39.github.io/",
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
